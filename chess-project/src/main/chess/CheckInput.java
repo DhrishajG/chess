12add2f32647875a1a5143f1fffb449d3ae50676
d66835dg <dhrishaj.garg@student.manchester.ac.uk>
2022-02-28 12:30:17 +0000

    changes made

--- chess-project/src/main/chess/CheckInput.java
+++ chess-project/src/main/chess/CheckInput.java
@@ -1,8 +1,15 @@
 package chess;
 
 public class CheckInput {
-	
+
 	public static boolean checkCoordinateValidity(String input){
-		return true;
+		char c1 = input.charAt(0);
+		char c2 = input.charAt(1);
+		if(c1 == '1' || c1 == '2' || c1 == '3' || c1 == '4' || c1 == '5' || c1 == '6' || c1 == '7' || c1 == '8'){
+			if(c2 == 'a' || c2 == 'b' || c2 == 'c' || c2 == 'd' || c2 == 'e' || c2 == 'f' || c2 == 'g' || c2 == 'h'){
+				return true;
+			}
+		}
+		return false;
 	}
 }
